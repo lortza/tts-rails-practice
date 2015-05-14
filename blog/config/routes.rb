523224aa63e943b4ef8318cf7fa5534c740c1582
anne <richardson.ae@gmail.com>
2015-05-14 14:18:37 -0500

    generated Post scaffold with title and content

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'page/about'
 
   get 'page/index'
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'page#about'
+  root 'page#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
