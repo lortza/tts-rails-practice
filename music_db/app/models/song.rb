dc304b7e6ec81ea5c9d7cb4f616fb2ad5f2934a3
anne <richardson.ae@gmail.com>
2015-05-19 10:48:52 -0500

    added validation

--- music_db/app/models/song.rb
+++ music_db/app/models/song.rb
@@ -3,6 +3,15 @@ class Song < ActiveRecord::Base
 
   validates :name, presence: true, length: {minimum: 1}
   validates :artist_id, presence: true
+  validates_uniqueness_of :name, :scope => :artist_id #this validates the song name within the scope of the artist id
+
+  #creating a recent scope that gives items created within the last indicated time
+  scope :recent, -> (minutes_past=60) {where("created_at > ?", minutes_past.minutes.ago)}
+
+#creating a scope with things created today
+  scope :today, -> {where("DATE(created_at) = ?", Date.today)}
+
+
 end
 
 # == Schema Information
