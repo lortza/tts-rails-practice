c5d0c69c9129b2898dd795da2f4160d5406c882b
anne <richardson.ae@gmail.com>
2015-06-11 10:29:06 -0500

    simple form changes

--- task_ajax/app/controllers/user_tasks_controller.rb
+++ task_ajax/app/controllers/user_tasks_controller.rb
@@ -2,7 +2,6 @@ class UserTasksController < ApplicationController
   before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_user_task, only: [:show, :edit, :update, :destroy]
 
-
   # GET /user_tasks/new
   def new
     @user_task = UserTask.new
