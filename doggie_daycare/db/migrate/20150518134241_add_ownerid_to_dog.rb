4f939321f87ce4a4b36912b830340f9ab59a0441
anne <richardson.ae@gmail.com>
2015-06-11 10:52:41 -0500

    i think i may have gotten rid of those crap files

--- doggie_daycare/db/migrate/20150518134241_add_ownerid_to_dog.rb
+++
@@ -1,5 +0,0 @@
-class AddOwneridToDog < ActiveRecord::Migration
-  def change
-    add_reference :dogs, :owner, index: true, foreign_key: true
-  end
-end
--- doggie_daycare/db/migrate/20150518135148_remove_owner_from_dogs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOwnerFromDogs < ActiveRecord::Migration
-  def change
-    remove_column :dogs, :owner, :string
-  end
-end
