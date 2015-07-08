ab93c6ecae7e0aead1b76a26030a79afb8be8379
anne <richardson.ae@gmail.com>
2015-07-08 12:56:07 -0500

    added type scaffold

--- critters/config/routes.rb
+++ critters/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :types
   resources :owners
   resources :countries
   # The priority is based upon order of creation: first created -> highest priority.
--- critters/db/schema.rb
+++ critters/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708175046) do
+ActiveRecord::Schema.define(version: 20150708175448) do
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
@@ -28,5 +28,11 @@ ActiveRecord::Schema.define(version: 20150708175046) do
   end
 
   add_index "owners", ["country_id"], name: "index_owners_on_country_id"
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
