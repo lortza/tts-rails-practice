5a255f9c4363b051528f1e3a919bd941ae3e4cf5
anne <richardson.ae@gmail.com>
2015-07-08 08:47:42 -0500

    set up basic nav

--- critterlog/config/routes.rb
+++ critterlog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :guardians
+  resources :countries
   resources :critters
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
