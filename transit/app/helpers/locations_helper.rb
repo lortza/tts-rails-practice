7759ab67e45e3adeb5b9a757f6463a108539b16c
anne <richardson.ae@gmail.com>
2015-07-09 14:35:06 -0500

    set up api pulling logic

--- transit/app/helpers/locations_helper.rb
+++ transit/app/helpers/locations_helper.rb
@@ -1,2 +1,15 @@
 module LocationsHelper
-end
+
+  # Parse the API data to store it in an array of hashes - each bus is a hash.
+  def fetch_api_data source
+    http = Net::HTTP.get_response(URI.parse(source))
+    data = http.body
+    JSON.parse(data)
+  end #fetch
+
+  # Compare latitude/longitude of the user and all the buses to see if they are
+  # within 0.01 degree
+  def is_nearby(lat_user, long_user, lat_bus, long_bus)
+    (long_user - long_bus).abs <= 0.01 && (lat_user - lat_bus).abs <= 0.01
+  end #nearby
+end #module
